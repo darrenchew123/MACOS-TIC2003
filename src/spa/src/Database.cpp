075520ff21cb6656f668abbada50be4ab2f6fb4b
darrenchew123 <darren09chew99@gmail.com>
2024-03-02 18:58:07 +0800

    DB related done

--- src/spa/src/Database.cpp
+++ src/spa/src/Database.cpp
@@ -39,9 +39,7 @@ void Database::initialize() {
     const char* createVariableTableSQL = "CREATE TABLE Variable ("
                                          "variableName VARCHAR(255) PRIMARY KEY,"
                                          "statementCodeLine INT,"
-                                         "procedureName VARCHAR(255),"
-                                         "FOREIGN KEY (statementCodeLine) REFERENCES Statement(codeLine),"
-                                         "FOREIGN KEY (procedureName) REFERENCES Procedure(procedureName));";
+                                         "FOREIGN KEY (statementCodeLine) REFERENCES Statement(codeLine));";
     sqlite3_exec(dbConnection, createVariableTableSQL, NULL, 0, &errorMessage);
 
     // create Constant table
@@ -96,6 +94,14 @@ void Database::initialize() {
     dbResults = vector<vector<string>>();
 }
 
+void Database::postProcessDbResults(vector<string>& results, int columnIndex) {
+    for (const vector<string>& dbRow : dbResults) {
+        if (dbRow.size() > columnIndex) {
+            results.push_back(dbRow.at(columnIndex));
+        }
+    }
+}
+
 void Database::clear() {
     // Delete all records from each table
     const char* clearTablesSQL = "DELETE FROM Uses;"
@@ -133,11 +139,7 @@ void Database::getProcedures(vector<string>& results){
     sqlite3_exec(dbConnection, getProceduresSQL.c_str(), callback, 0, &errorMessage);
 
     // postprocess the results from the database so that the output is just a vector of procedure names
-    for (vector<string> dbRow : dbResults) {
-        string result;
-        result = dbRow.at(0);
-        results.push_back(result);
-    }
+    postProcessDbResults(results,0);
 }
 
 // method to get insert statement into the database
@@ -157,10 +159,7 @@ void Database::getStatements(vector<string>& results) {
     string getStatementsSQL = "SELECT DISTINCT codeLine FROM Statement;";
     sqlite3_exec(dbConnection, getStatementsSQL.c_str(), callback, 0, &errorMessage);
 
-    for (vector<string> dbRow : dbResults) {
-        string statementLine = dbRow.at(0);
-        results.push_back(statementLine);
-    }
+    postProcessDbResults(results,0);
 }
 
 // method to get all the statements line from the database by statement type eg. print, push
@@ -170,17 +169,14 @@ void Database::getStatementType(const string &statementType, vector<string>& res
                                + statementType + "';";
     sqlite3_exec(dbConnection, getStatementsSQL.c_str(), callback, 0, &errorMessage);
 
-    for (vector<string> dbRow : dbResults) {
-        string statementLine = dbRow.at(0);
-        results.push_back(statementLine);
-    }
+    postProcessDbResults(results,0);
 }
 
 // method to insert a variable into the database
-void Database::insertVariable(string variableName, int codeLine) {
-    string insertVariableSQL = "INSERT INTO Variable (variableName, codeLine) VALUES ('"
+void Database::insertVariable(string variableName, int statementCodeLine) {
+    string insertVariableSQL = "INSERT INTO Variable (variableName, statementCodeLine) VALUES ('"
                                + variableName + "', '"
-                               + to_string(codeLine) + "');";
+                               + to_string(statementCodeLine) + "');";
     sqlite3_exec(dbConnection, insertVariableSQL.c_str(), NULL, 0, &errorMessage);
 }
 
@@ -191,16 +187,13 @@ void Database::getVariables(vector<string>& results) {
     string getVariablesSQL = "SELECT DISTINCT variableName FROM Variable;";
     sqlite3_exec(dbConnection, getVariablesSQL.c_str(), callback, 0, &errorMessage);
 
-    for (vector<string> dbRow : dbResults) {
-        string variableName = dbRow.at(0);
-        results.push_back(variableName);
-    }
+    postProcessDbResults(results,0);
 }
 
 // method to insert a constant into the database
-void Database::insertConstant(int codeLine, int constantValue) {
-    string insertConstantSQL = "INSERT INTO Constant (codeLine,constantValue) VALUES ("
-                               + to_string(codeLine) + ","
+void Database::insertConstant(int statementCodeLine, int constantValue) {
+    string insertConstantSQL = "INSERT INTO Constant (statementCodeLine,constantValue) VALUES ("
+                               + to_string(statementCodeLine) + ","
                                + to_string(constantValue) + ");";
 
     sqlite3_exec(dbConnection, insertConstantSQL.c_str(), NULL, 0, &errorMessage);
@@ -213,11 +206,82 @@ void Database::getConstants(vector<string>& results) {
     string getConstantsSQL = "SELECT constantValue FROM Constant;";
     sqlite3_exec(dbConnection, getConstantsSQL.c_str(), callback, 0, &errorMessage);
 
-    for (vector<string> dbRow : dbResults) {
-        string constant = dbRow.at(0);
-        results.push_back(constant);
-    }
+    postProcessDbResults(results,0);
 }
+
+void Database::insertParentChildRelation(int parentStatementCodeLine, int childStatementCodeLine) {
+    string insertSQL = "INSERT INTO ParentChildRelation (parentStatementCodeLine, childStatementCodeLine) VALUES ("
+                       + to_string(parentStatementCodeLine) + ", "
+                       + to_string(childStatementCodeLine) + ");";
+    sqlite3_exec(dbConnection, insertSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+void Database::getParentChildRelations(vector<string>& results) {
+    dbResults.clear();
+    string getSQL = "SELECT parentStatementCodeLine, childStatementCodeLine FROM ParentChildRelation;";
+    sqlite3_exec(dbConnection, getSQL.c_str(), callback, 0, &errorMessage);
+    postProcessDbResults(results,0);
+}
+
+void Database::insertNextRelation(int currentStatementCodeLine, int nextStatementCodeLine) {
+    string insertSQL = "INSERT INTO NextRelation (currentStatementCodeLine, nextStatementCodeLine) VALUES ("
+                       + to_string(currentStatementCodeLine) + ", "
+                       + to_string(nextStatementCodeLine) + ");";
+    sqlite3_exec(dbConnection, insertSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+void Database::getNextRelations(vector<string>& results) {
+    dbResults.clear();
+    string getSQL = "SELECT currentStatementCodeLine, nextStatementCodeLine FROM NextRelation;";
+    sqlite3_exec(dbConnection, getSQL.c_str(), callback, 0, &errorMessage);
+    postProcessDbResults(results,0);
+}
+
+void Database::insertUses(int statementCodeLine, const string& variableName) {
+    string insertSQL = "INSERT INTO Uses (statementCodeLine, variableName) VALUES ("
+                       + to_string(statementCodeLine) + ", '"
+                       + variableName + "');";
+    sqlite3_exec(dbConnection, insertSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+void Database::getUses(vector<string>& results) {
+    dbResults.clear();
+    string getSQL = "SELECT statementCodeLine, variableName FROM Uses;";
+    sqlite3_exec(dbConnection, getSQL.c_str(), callback, 0, &errorMessage);
+    postProcessDbResults(results,0);
+}
+
+void Database::insertModifies(int statementCodeLine, const string& variableName) {
+    string insertSQL = "INSERT INTO Modifies (statementCodeLine, variableName) VALUES ("
+                       + to_string(statementCodeLine) + ", '"
+                       + variableName + "');";
+    sqlite3_exec(dbConnection, insertSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+void Database::getModifies(vector<string>& results) {
+    dbResults.clear();
+    string getSQL = "SELECT statementCodeLine, variableName FROM Modifies;";
+    sqlite3_exec(dbConnection, getSQL.c_str(), callback, 0, &errorMessage);
+    postProcessDbResults(results,0);
+}
+
+void Database::insertPattern(int statementCodeLine, const string& LHSExpression, const string& RHSExpression) {
+    string insertSQL = "INSERT INTO Pattern (statementCodeLine, LHSExpression, RHSExpression) VALUES ("
+                       + to_string(statementCodeLine) + ", '"
+                       + LHSExpression + "', '"
+                       + RHSExpression + "');";
+    sqlite3_exec(dbConnection, insertSQL.c_str(), NULL, 0, &errorMessage);
+}
+
+void Database::getPatterns(vector<string>& results) {
+    dbResults.clear();
+    string getSQL = "SELECT statementCodeLine, LHSExpression, RHSExpression FROM Pattern;";
+    sqlite3_exec(dbConnection, getSQL.c_str(), callback, 0, &errorMessage);
+    postProcessDbResults(results,2);
+}
+
+
+
 
 
 // callback method to put one row of results from the database into the dbResults vector
--- src/unit_testing/src/TestDatabase.cpp
+++ src/unit_testing/src/TestDatabase.cpp
@@ -71,9 +71,9 @@ namespace TestDatabase {
         for (const auto& variable : dbResults) {
             testOutput.append(variable + ",");
         }
-
+        cout << "Test Output: "<<testOutput << endl;
         string expectedOutput = "var1,var2,";
-        cout << testOutput << endl;
+
         require(testOutput == expectedOutput);
     }
 
@@ -85,12 +85,110 @@ namespace TestDatabase {
         vector<string> dbResults;
         Database::getConstants(dbResults);
 
+
         string testOutput;
         for (const auto& constantLine : dbResults) {
             testOutput.append(constantLine + "$");
         }
+        cout <<"Test Output: "<<testOutput << endl;
 
         string expectedOutput = "100$200$";
+
+
+        require(testOutput == expectedOutput);
+    }
+
+    TEST_CASE("CheckDatabaseParentChildRelation") {
+        Database::initialize();
+        Database::insertParentChildRelation(1, 2);
+        Database::insertParentChildRelation(2, 3);
+
+        vector<string> dbResults;
+        Database::getParentChildRelations(dbResults);
+
+        string testOutput;
+        for (const auto& relation : dbResults) {
+            testOutput.append(relation + "$");
+        }
+        cout <<"Test Output: "<<testOutput << endl;
+
+        string expectedOutput = "1$2$"; // Assuming the format "parent-child"
+
+        require(testOutput == expectedOutput);
+    }
+
+    TEST_CASE("CheckDatabaseNextRelation") {
+        Database::initialize();
+        Database::insertNextRelation(1, 2);
+        Database::insertNextRelation(2, 3);
+
+        vector<string> dbResults;
+        Database::getNextRelations(dbResults);
+
+        string testOutput;
+        for (const auto& relation : dbResults) {
+            testOutput.append(relation + "$");
+        }
+        cout <<"Test Output: "<<testOutput << endl;
+
+        string expectedOutput = "1$2$";
+
+        require(testOutput == expectedOutput);
+    }
+
+    TEST_CASE("CheckDatabaseUses") {
+        Database::initialize();
+        Database::insertUses(1, "varA");
+        Database::insertUses(2, "varB");
+
+        vector<string> dbResults;
+        Database::getUses(dbResults);
+
+        string testOutput;
+        for (const auto& use : dbResults) {
+            testOutput.append(use + "$");
+        }
+        cout <<"Test Output: "<<testOutput << endl;
+
+        string expectedOutput = "1$2$";
+
+        require(testOutput == expectedOutput);
+    }
+
+    TEST_CASE("CheckDatabaseModifies") {
+        Database::initialize();
+        Database::insertModifies(1, "varX");
+        Database::insertModifies(2, "varY");
+
+        vector<string> dbResults;
+        Database::getModifies(dbResults);
+
+        string testOutput;
+        for (const auto& modify : dbResults) {
+            testOutput.append(modify + "$");
+        }
+        cout <<"Test Output: "<<testOutput << endl;
+
+        string expectedOutput = "1$2$";
+
+        require(testOutput == expectedOutput);
+    }
+
+    TEST_CASE("CheckDatabasePattern") {
+        Database::initialize();
+        Database::insertPattern(1, "LHS1", "RHS1");
+        Database::insertPattern(2, "LHS2", "RHS2");
+
+        vector<string> dbResults;
+        Database::getPatterns(dbResults);
+
+        string testOutput;
+        for (const auto& pattern : dbResults) {
+            testOutput.append(pattern + "$");
+        }
+        cout <<"Test Output: "<<testOutput << endl;
+
+        string expectedOutput = "RHS1$RHS2$";
 
         require(testOutput == expectedOutput);
     }
